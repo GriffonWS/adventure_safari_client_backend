80586c7518a14373e327fe77f6e8179c6197b5d1
Mohd Ubaidullah <devubaid493@gmail.com>
2025-06-03 12:47:23 +0530

    first commit

--- server.js
+++ server.js
@@ -11,12 +11,7 @@ const authRoutes = require('./routes/auth');
 const app = express();
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+connectDB();
 
 // Middleware
 app.use(cors({
