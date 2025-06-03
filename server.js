5bbaaaa9b15a7401b84f70b19d35d1c6ab1932c3
Mohd Ubaidullah <devubaid493@gmail.com>
2025-06-03 12:48:21 +0530

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 
 // Initialize app
